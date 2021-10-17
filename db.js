061bda7516b69ee26b0c49afd83b23459b88cf5e
Burak Tahtacıoğlu <clubwomanizer588@gmail.com>
2021-10-17 15:15:41 +0300

    init

--- db.js
+++ db.js
@@ -9,8 +9,14 @@ module.exports = connectToDatabase = () => {
   }
 
   console.log('=> using new database connection');
-  return mongoose.connect(process.env.DB)
+  return mongoose.connect("mongodb+srv://parklab-user:EzxE2u66yyDNPPE5@parklab-shared-cluster.mjnyy.mongodb.net/parklab-dev",{
+    // useNewUrlParser: true
+    // useUnifiedTopology: true
+    // useFindAndModify: false,
+    // useCreateIndex: true
+    })
     .then(db => { 
       isConnected = db.connections[0].readyState;
-    });
+      console.log(isConnected);
+    }).catch(err => console.log(err));
 };
